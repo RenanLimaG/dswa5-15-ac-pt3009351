08c3930fff189369905c0a2bd59caba1e2c3eb54
RenanLimaG <renanlimag10@gmail.com>
2022-06-08 21:32:05 -0300

    Teste

--- config/passport.js
+++ config/passport.js
@@ -1,35 +1,37 @@
 var passport = require('passport');
-var GitHubStrategy = require('passport-github2').Strategy;
+var GitHubStrategy = require('passport-github').Strategy;
+var findOrCreate = require('mongoose-findorcreate');
 var mongoose = require('mongoose');
 
 module.exports = function() {
+
     var Usuario = mongoose.model('Usuario');
 
     passport.use(new GitHubStrategy({
         clientID: '43ceaba8bea0a10020fd',
-        clientSecret: '8e30b22e1ccf48faff9421c8a511cae361172c1d',
+        clientSecret: '64d902db35f77f0e274f9e03f74bce9cd7242ddf',
         callbackURL: 'https://dswa5-15-ac-pt3009351.herokuapp.com/auth/github/callback'
     }, function(accessToken, refreshToken, profile, done) {
-        Usuario.findOrCreate(
-            { "login" : profile.username},
-            { "nome" : profile.username},
+
+        Usuario.findOrCreate({ "login": profile.username }, { "nome": profile.username },
             function(erro, usuario) {
-            if(erro){
-                console.log(erro);
-                return done(erro);
-            }
-            return done(null, usuario);
+                if (erro) {
+                    return done(erro);
+                }
+                return done(null, usuario);
             }
         );
     }));
+
     passport.serializeUser(function(usuario, done) {
         done(null, usuario._id);
     });
+
     passport.deserializeUser(function(id, done) {
         Usuario.findById(id).exec()
-        .then(function(usuario) {
-        done(null, usuario);
-        });
+            .then(function(usuario) {
+                done(null, usuario);
+            });
     });
 };
 
